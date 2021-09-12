8730b450d2b168006d289d0298b243b1a208ac3b
ted <teddyliangfh@gmail.com>
2021-09-12 13:05:38 +1000

    format

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import Cell from '../components/cell';
-import { CellType } from "../interfaces/cell-interface"
+import Cell from "../components/cell";
+import { CellType } from "../interfaces/cell-interface";
 
 const StyledGameBox = styled.div`
   display: flex;
@@ -12,15 +12,15 @@ const StyledGameBox = styled.div`
   width: 40em;
   height: 30em;
   margin: auto;
-  border-radius: .5em;
+  border-radius: 0.5em;
   .game-area {
     width: 20em;
     height: 20em;
     background: #fff;
     margin: auto;
     display: grid;
-    gap: .2em;
-    padding: .6em;
+    gap: 0.2em;
+    padding: 0.6em;
     grid-template-columns: repeat(10, auto);
     margin: auto;
     border-radius: 0.2em;
@@ -30,21 +30,22 @@ const StyledGameBox = styled.div`
   }
 `;
 const Button = styled.button`
-   color: #fff;
-    height: 2em;
-    font-size: 1em;
-    width: 8.8em;
-    background-color: rgb(25, 44, 56);
-    border-radius: 0.25rem;
-    border: 1px solid transparent;
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-    margin-left: 0.5em;
-    margin-right: 0.5em;
-    cursor: pointer;
-    user-select: none;
+  color: #fff;
+  height: 2em;
+  font-size: 1em;
+  width: 8.8em;
+  background-color: rgb(25, 44, 56);
+  border-radius: 0.25rem;
+  border: 1px solid transparent;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 0.5em;
+  margin-right: 0.5em;
+  cursor: pointer;
+  user-select: none;
 `;
+
 interface StateType {
   cellsArr: CellType[];
 }
@@ -62,8 +63,8 @@ class Game extends React.Component {
     let key = 0;
     for (let i = 0; i < 10; i++) {
       for (let j = 0; j < 10; j++) {
-        initArr.push({ id: key, isAlive: false, x: j, y: i, });
-        key++
+        initArr.push({ id: key, isAlive: false, x: j, y: i });
+        key++;
       }
     }
     this.setState({
@@ -75,14 +76,16 @@ class Game extends React.Component {
     this.updateCells(aim);
   }
 
-  updateCells(id: number | undefined) {
+  updateCells(id: number) {
     const { cellsArr } = this.state;
-    const upDatedCellArray = cellsArr && cellsArr.map((item) => {
-      if (item.id === id) {
-        item.isAlive = !item.isAlive
-      }
-      return item
-    })
+    const upDatedCellArray =
+      cellsArr &&
+      cellsArr.map((item) => {
+        if (item.id === id) {
+          item.isAlive = !item.isAlive;
+        }
+        return item;
+      });
     this.setState({ cellsArr: upDatedCellArray });
   }
 
@@ -92,12 +95,12 @@ class Game extends React.Component {
 
   nextGeneration() {
     const { cellsArr } = this.state;
-    //deep clone 
+    //deep clone
     let arrayCopy: [] = JSON.parse(JSON.stringify(cellsArr));
     const nexGenerationArr = arrayCopy.map((cell) => {
       const newCell = this.handleLife(cell);
       return newCell;
-    })
+    });
     this.setState({ cellsArr: nexGenerationArr });
   }
 
@@ -118,15 +121,15 @@ class Game extends React.Component {
     //A Cell with 2 or 3 live neighbours lives on to the next generation.
     if (isAlive && (aliveNeighbour === 2 || aliveNeighbour === 3)) {
       cell.isAlive = true;
-      return cell
+      return cell;
     }
     //An empty Cell with exactly 3 live neighbours "comes to life".
     if (isAlive === false && aliveNeighbour === 3) {
       cell.isAlive = true;
-      return cell
+      return cell;
     }
 
-    return cell
+    return cell;
   }
 
   // inspect Neighbours, and count how many alive cells
@@ -140,29 +143,24 @@ class Game extends React.Component {
         aliveNumber++;
       }
     }
-    return aliveNumber
+    return aliveNumber;
   }
 
   createCheckRule(cell: CellType) {
     const { x, y, id } = cell;
-    //inner cell: check 8 neigbours
-    if (x > 0 && x < 9 && y > 0 && y < 9) {
-      return [-11 + id, -10 + id, -9 + id, -1 + id, 1 + id, 9 + id, 10 + id, 11 + id]
-    } else {
-      // deal with the case:A Cell who "comes to life" outside the board should wrap at the other side of the board.
-      let checkArr = [];
-      for (let i = -1; i < 2; i++) {
-        for (let j = -1; j < 2; j++) {
-          let colIndex = (x + i + 10) % 10;
-          let rowIndex = (y + j + 10) % 10;
-          let idd = 10 * rowIndex + colIndex;
-          //remove the cell itself
-          if (idd !== id) { checkArr.push(idd); }
+    let checkArr = [];
+    for (let i = -1; i < 2; i++) {
+      for (let j = -1; j < 2; j++) {
+        let colIndex = (x + i + 10) % 10;
+        let rowIndex = (y + j + 10) % 10;
+        let neighbourId = 10 * rowIndex + colIndex;
+        //remove the cell itself
+        if (neighbourId !== id) {
+          checkArr.push(neighbourId);
         }
       }
-      // if (cell.isAlive) console.log('checkArr', cell, checkArr);
-      return checkArr
     }
+    return checkArr;
   }
 
   render() {
@@ -170,13 +168,16 @@ class Game extends React.Component {
 
     return (
       <StyledGameBox>
-        <h3>
-          Cell Simulator Life Game
-        </h3>
+        <h3>Cell Simulator Life Game</h3>
         <div className="game-area">
           {cellsArr.map((item) => {
             return (
-              <div key={item?.id} onClick={() => { this.handleClickCell(item?.id) }} >
+              <div
+                key={item?.id}
+                onClick={() => {
+                  this.handleClickCell(item?.id);
+                }}
+              >
                 <Cell
                   id={item?.id}
                   x={item.x}
@@ -188,11 +189,19 @@ class Game extends React.Component {
             );
           })}
         </div>
-        <div className="button-area" >
-          <Button onClick={() => { this.resetCells() }}>
+        <div className="button-area">
+          <Button
+            onClick={() => {
+              this.resetCells();
+            }}
+          >
             reset
           </Button>
-          <Button onClick={() => { this.nextGeneration() }}>
+          <Button
+            onClick={() => {
+              this.nextGeneration();
+            }}
+          >
             next generation
           </Button>
         </div>
